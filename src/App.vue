014835425bc49c35042e96f4ab56884b348c1f81
Stefan Boos <stefan.boos@gmx.de>
2019-03-16 14:55:18 +0100

    Created feature folders + TrackLog component

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,26 @@
     <header>
       <nav class="nav nav-tabs">
           <a class="nav-item nav-link active" href="#">
-            <img src="./assets/16x16/status/appointment-soon.png" alt="Logo" /> Home
+            <img src="./assets/16x16/status/appointment-soon.png" alt="Logo" /> Start
           </a>
-          <a class="nav-item nav-link" href="#">Settings</a>
-          <a class="nav-item nav-link" href="#">About</a>
+          <a class="nav-item nav-link" href="#">Einstellungen</a>
+          <a class="nav-item nav-link" href="#">Hilfe</a>
       </nav>
     </header>
     <main>
-      <HomePage />
+      <TrackLog />
     </main>
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+// import HomePage from './home/HomePage.vue';
+import TrackLog from './connection/TrackLog.vue';
 
 export default {
   name: 'app',
   components: {
-    HomePage,
+    TrackLog,
   },
 };
 </script>
---
+++ src/connection/TrackLog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">Diese Verbindung wird beobachtet:</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TrackLog',
+};
+</script>
+
+
+<style>
+</style>
