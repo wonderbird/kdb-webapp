99c6afc3e2c4e89300e860ec143bc3ff7e13de2b
Stefan Boos <sboos@dimedis.de>
2019-05-09 09:32:18 +0200

    feat: get geolocation (wip)

--- tests/unit/home/HomePage.test.js
+++ tests/unit/home/HomePage.test.js
@@ -5,18 +5,29 @@ describe('HomePage', () => {
   describe('coords()', () => {
     const wrapper = shallowMount(HomePage);
 
-    const mockGeolocation = {
-      getCurrentPosition: jest.fn((success, error) => error()),
-      watchPosition: jest.fn(),
-    };
-
-    navigator.geolocation = mockGeolocation;
-
     it('returns { 48, 2 }', () => {
-      expect(wrapper.vm.coords).toEqual({ latitude: 48, longitude: 2 });
+      const position = { latitude: 48, longitude: 2 };
+
+      const mockGeolocation = {
+        getCurrentPosition: jest.fn(success => success(position)),
+      };
+
+      navigator.geolocation = mockGeolocation;
+
+      const coords = wrapper.vm.coords;
+
+      expect(mockGeolocation.getCurrentPosition.mock.calls.length).toBe(1);
+      expect(coords).toEqual({ latitude: 48, longitude: 2 });
     });
 
     it('when geolocation rejected then returns undefined', () => {
+      const mockGeolocation = {
+        getCurrentPosition: jest.fn((success, error) => error()),
+      };
+
+      navigator.geolocation = mockGeolocation;
+
+      expect(mockGeolocation.getCurrentPosition.mock.calls.length).toBe(1);
       expect(wrapper.vm.coords).toEqual({ latitude: undefined, longitude: undefined });
     });
   });
