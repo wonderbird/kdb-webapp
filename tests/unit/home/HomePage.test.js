1565b2b4cba0df75906bd4345ad798cb686a953f
Stefan Boos <stefan.boos@gmx.de>
2019-05-09 05:27:36 +0200

    test: fix unit tests - missing files in git

--- tests/unit/home/HomePage.test.js
+++ tests/unit/home/HomePage.test.js
@@ -1,5 +1,5 @@
-import { shallowMount } from '@vue/test-utils'
-import HomePage from '../../src/home/HomePage';
+import { shallowMount } from '@vue/test-utils';
+import HomePage from '../../../src/home/HomePage.vue';
 
 test('coords returns { 48, 2 }', () => {
   const wrapper = shallowMount(HomePage);
